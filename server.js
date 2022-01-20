e2fa618f5bc26393f93d4a8d9c9345c71ec33cb0
Buddy Agyin <agyin3@gmail.com>
2022-01-20 13:48:39 -0700

    add in homepage success result

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.get("/", (req, res) => {
+    res.send("IT IS ALIVE!!!");
+});
 app.use("/", Router);
 
 // Global error handling
